8492544e098d05d6152690c38013fa48101f978e
Fyroo <chaouimoutaa@gmail.com>
2021-08-22 18:04:20 +0100

    token

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
 const { Client,  Intents } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, ] });
+const { token } = require('./config.json');
+client.login(token);
